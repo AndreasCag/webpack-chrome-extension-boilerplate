542c5d11a979073babba4a448583af731242fc56
Andrew Anikin <andreas97fm@mail.ru>
2018-01-01 16:48:54 +0300

    Babel fix

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,3 +1,5 @@
+import testMessage from './test-workers/worker';
 import '@/stylus/main.styl';
 
+testMessage();
 console.log('test');
---
+++ src/js/test-workers/worker.js
@@ -0,0 +1,3 @@
+export default function() {
+  console.log('test message from worker');
+}
